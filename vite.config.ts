9856e2a528cb2dfcdfd8cff3fd07721e76828b7b
Silvio Donizeti De Mattos Junior <Silvio.donizeti.jr@outlook.com>
2025-10-31 18:14:42 -0300

    feat: Add GitHub Actions deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CV-Portfolio/', 
+  // << ADICIONE ESTA LINHA >>
+  base: '/CV-SILVIO/' // << IMPORTANTE: Substitua pelo nome do seu repositório no GitHub
 })
